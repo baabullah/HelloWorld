2f3eef2ddf1e9a6ec6ea0cf405505f5127a9c6dc
baabullah <baabullah@yahoo.com>
2023-06-06 23:14:43 +0700

    update

--- contentscript.js
+++ contentscript.js
@@ -1,8 +1,16 @@
-var s = document.createElement('script');
-s.src = chrome.runtime.getURL('inject.js');
-s.onload = function() {
+function loadInject() {
+	var s = document.createElement('script');
+	s.src = chrome.runtime.getURL("inject.js");
+	s.onload = function() {
+		this.remove();
+	};
+	(document.head || document.documentElement).appendChild(s);
+}
+
+var s2 = document.createElement('script');
+s2.src = chrome.runtime.getURL('jquery-3.6.0.min.js');
+s2.onload = function() {
+	loadInject();
 	this.remove();
 };
-(document.head || document.documentElement).appendChild(s);
-
-
+(document.head || document.documentElement).appendChild(s2);

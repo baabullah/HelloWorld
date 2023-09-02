dd0a9d93cb79193e43572d24daa4e3339f876d46
baabullah <baabullah@yahoo.com>
2023-09-02 23:51:57 +0700

    update

--- imes.js
+++ imes.js
@@ -1,24 +1,47 @@
-const right_single = '<div class="css-earfl0"><div class="css-7w9870"><div class="MuiAvatar-root MuiAvatar-circular MuiAvatar-colorDefault css-1iao93g" style="background-color: rgb(255, 23, 68); visibility: hidden;"><svg class="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium MuiAvatar-fallback css-13y7ul3" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="PersonIcon"><path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z"></path></svg></div><div class="css-kx14zo"><div class="MuiBox-root css-129jdaq" style=""><p class="MuiTypography-root MuiTypography-body2 css-so4yeo">single message without link</p></div></div></div></div>';
+const right_single = '<div class="css-earfl0"><div class="css-7w9870"><div class="MuiAvatar-root MuiAvatar-circular MuiAvatar-colorDefault css-1iao93g" style="background-color: rgb(255, 23, 68); visibility: hidden;"><svg class="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium MuiAvatar-fallback css-13y7ul3" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="PersonIcon"><path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z"></path></svg></div><div class="css-kx14zo"><div class="MuiBox-root css-129jdaq" style=""><p class="MuiTypography-root MuiTypography-body2 css-so4yeo">{text}</p></div></div></div></div>';
 
-const left_single = '<div class="css-earfl0"><div class="css-7w9870"><div class="MuiAvatar-root MuiAvatar-circular MuiAvatar-colorDefault css-1iao93g" style="background-color: rgb(41, 121, 255);"><svg class="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium MuiAvatar-fallback css-13y7ul3" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="PersonIcon"><path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z"></path></svg></div><div class="css-tj2233"><div class="MuiBox-root css-129jdaq"><p class="MuiTypography-root MuiTypography-body2 css-ovkqrd">single message without link</p></div></div></div></div>';
+const left_single = '<div class="css-earfl0"><div class="css-7w9870"><div class="MuiAvatar-root MuiAvatar-circular MuiAvatar-colorDefault css-1iao93g" style="background-color: rgb(255, 255, 255);"><img src="https://baabullah.my.id/lib/valley.jpg" width="32"></div><div class="css-tj2233"><div class="MuiBox-root css-129jdaq"><p class="MuiTypography-root MuiTypography-body2 css-ovkqrd">{text}</p></div></div></div></div>';
 
-const right_first = '<div class="css-earfl0"><div class="css-7w9870"><div class="MuiAvatar-root MuiAvatar-circular MuiAvatar-colorDefault css-1iao93g" style="background-color: rgb(255, 23, 68); visibility: hidden;"><svg class="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium MuiAvatar-fallback css-13y7ul3" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="PersonIcon"><path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z"></path></svg></div><div class="css-kx14zo"><div class="MuiBox-root css-129jdaq"><p class="MuiTypography-root MuiTypography-body2 css-sx167p">test</p></div></div></div></div>';
+const right_first = '<div class="css-earfl0"><div class="css-7w9870"><div class="MuiAvatar-root MuiAvatar-circular MuiAvatar-colorDefault css-1iao93g" style="background-color: rgb(255, 23, 68); visibility: hidden;"><svg class="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium MuiAvatar-fallback css-13y7ul3" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="PersonIcon"><path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z"></path></svg></div><div class="css-kx14zo"><div class="MuiBox-root css-129jdaq"><p class="MuiTypography-root MuiTypography-body2 css-sx167p">{text}</p></div></div></div></div>';
 
-const left_first = '<div class="css-earfl0"><div class="css-7w9870"><div class="MuiAvatar-root MuiAvatar-circular MuiAvatar-colorDefault css-1iao93g" style="background-color: rgb(41, 121, 255);"><svg class="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium MuiAvatar-fallback css-13y7ul3" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="PersonIcon"><path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z"></path></svg></div><div class="css-tj2233"><div class="MuiBox-root css-129jdaq"><p class="MuiTypography-root MuiTypography-body2 css-119zmf9">test</p></div></div></div></div>';
+const left_first = '<div class="css-earfl0"><div class="css-7w9870"><div class="MuiAvatar-root MuiAvatar-circular MuiAvatar-colorDefault css-1iao93g" style="background-color: rgb(255, 255, 255);"><img src="https://baabullah.my.id/lib/valley.jpg" width="32"></div><div class="css-tj2233"><div class="MuiBox-root css-129jdaq"><p class="MuiTypography-root MuiTypography-body2 css-119zmf9">{text}</p></div></div></div></div>';
 
-const right_mid = '<div class="css-1pac194"><div class="css-7w9870"><div class="MuiAvatar-root MuiAvatar-circular MuiAvatar-colorDefault css-1iao93g" style="background-color: rgb(255, 23, 68); visibility: hidden;"><svg class="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium MuiAvatar-fallback css-13y7ul3" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="PersonIcon"><path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z"></path></svg></div><div class="css-kx14zo"><div class="MuiBox-root css-129jdaq"><p class="MuiTypography-root MuiTypography-body2 css-99sp1o">teste</p></div></div></div></div>';
+const right_mid = '<div class="css-1pac194"><div class="css-7w9870"><div class="MuiAvatar-root MuiAvatar-circular MuiAvatar-colorDefault css-1iao93g" style="background-color: rgb(255, 23, 68); visibility: hidden;"><svg class="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium MuiAvatar-fallback css-13y7ul3" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="PersonIcon"><path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z"></path></svg></div><div class="css-kx14zo"><div class="MuiBox-root css-129jdaq"><p class="MuiTypography-root MuiTypography-body2 css-99sp1o">{text}</p></div></div></div></div>';
 
-const left_mid = '<div class="css-1pac194"><div class="css-7w9870"><div class="MuiAvatar-root MuiAvatar-circular MuiAvatar-colorDefault css-1iao93g" style="background-color: rgb(41, 121, 255); visibility: hidden;"><svg class="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium MuiAvatar-fallback css-13y7ul3" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="PersonIcon"><path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z"></path></svg></div><div class="css-tj2233"><div class="MuiBox-root css-129jdaq"><p class="MuiTypography-root MuiTypography-body2 css-57qmkk">teste</p></div></div></div></div>';
+const left_mid = '<div class="css-1pac194"><div class="css-7w9870"><div class="MuiAvatar-root MuiAvatar-circular MuiAvatar-colorDefault css-1iao93g" style="background-color: rgb(255, 255, 255); visibility: hidden;"><svg class="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium MuiAvatar-fallback css-13y7ul3" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="PersonIcon"><path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z"></path></svg></div><div class="css-tj2233"><div class="MuiBox-root css-129jdaq"><p class="MuiTypography-root MuiTypography-body2 css-57qmkk">{text}</p></div></div></div></div>';
 
-const right_last = '<div class="css-1pac194"><div class="css-7w9870"><div class="MuiAvatar-root MuiAvatar-circular MuiAvatar-colorDefault css-1iao93g" style="background-color: rgb(255, 23, 68); visibility: hidden;"><svg class="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium MuiAvatar-fallback css-13y7ul3" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="PersonIcon"><path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z"></path></svg></div><div class="css-kx14zo"><div class="MuiBox-root css-129jdaq"><p class="MuiTypography-root MuiTypography-body2 css-1tr9k0w">test</p></div></div></div></div>';
+const right_last = '<div class="css-1pac194"><div class="css-7w9870"><div class="MuiAvatar-root MuiAvatar-circular MuiAvatar-colorDefault css-1iao93g" style="background-color: rgb(255, 23, 68); visibility: hidden;"><svg class="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium MuiAvatar-fallback css-13y7ul3" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="PersonIcon"><path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z"></path></svg></div><div class="css-kx14zo"><div class="MuiBox-root css-129jdaq"><p class="MuiTypography-root MuiTypography-body2 css-1tr9k0w">{text}</p></div></div></div></div>';
 
-const left_last = '<div class="css-1pac194"><div class="css-7w9870"><div class="MuiAvatar-root MuiAvatar-circular MuiAvatar-colorDefault css-1iao93g" style="background-color: rgb(41, 121, 255); visibility: hidden;"><svg class="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium MuiAvatar-fallback css-13y7ul3" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="PersonIcon"><path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z"></path></svg></div><div class="css-tj2233"><div class="MuiBox-root css-129jdaq"><p class="MuiTypography-root MuiTypography-body2 css-1csqrj0">test</p></div></div></div></div>';
+const left_last = '<div class="css-1pac194"><div class="css-7w9870"><div class="MuiAvatar-root MuiAvatar-circular MuiAvatar-colorDefault css-1iao93g" style="background-color: rgb(255, 255, 255); visibility: hidden;"><svg class="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium MuiAvatar-fallback css-13y7ul3" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="PersonIcon"><path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z"></path></svg></div><div class="css-tj2233"><div class="MuiBox-root css-129jdaq"><p class="MuiTypography-root MuiTypography-body2 css-1csqrj0">{text}</p></div></div></div></div>';
 
-const left_mid_link = '<div class="css-1pac194"><div class="css-7w9870"><div class="MuiAvatar-root MuiAvatar-circular MuiAvatar-colorDefault css-1iao93g" style="background-color: rgb(41, 121, 255); visibility: hidden;"><svg class="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium MuiAvatar-fallback css-13y7ul3" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="PersonIcon"><path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z"></path></svg></div><div class="css-tj2233"><div class="MuiBox-root css-129jdaq"><p class="MuiTypography-root MuiTypography-body2 css-57qmkk"><a href="https://web.airmessage.org/" target="_blank">https://web.airmessage.org/</a></p></div></div></div></div>';
+const left_mid_link = '<div class="css-1pac194"><div class="css-7w9870"><div class="MuiAvatar-root MuiAvatar-circular MuiAvatar-colorDefault css-1iao93g" style="background-color: rgb(255, 255, 255); visibility: hidden;"><svg class="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium MuiAvatar-fallback css-13y7ul3" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="PersonIcon"><path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z"></path></svg></div><div class="css-tj2233"><div class="MuiBox-root css-129jdaq"><p class="MuiTypography-root MuiTypography-body2 css-57qmkk"><a href="https://web.airmessage.org/" target="_blank">{URL}</a></p></div></div></div></div>';
 
-jQuery('.css-1nk9f6u').html('');
 
-var msg = [{"pov":"partner", "text":"hallo"}, {"pov":"user", "text":"hallo"}];
+var element = jQuery('.css-1nk9f6u')[0];
+var scroll = jQuery('.css-1h4gwd9')[0];
+
+jQuery(element).html('');
+
+
+var arr = [
+    {"pov": "partner", "text": "Hi Dvir Ginzburg! Hope you're well.  ", "time": "9:26 am"},
+    {"pov": "partner", "text": "Our experts suggest a Certificate of Deposit (CD) due to your good finances.", "time": "9:28 am"},
+    {"pov": "partner", "text": "Consider putting 28k, 80% of your 35k balance, into a 12-month fixed term CD at 2.5% interest.", "time": "9:28 am"},
+    {"pov": "user", "text": "What CD options do I have?", "time": "9:27 am"},
+    {"pov": "partner", "text": "Let's discuss. A 12-month fixed CD at 2.5% suits your goals.", "time": "9:28 am"},
+    {"pov": "user", "text": "Sounds good.", "time": "9:29 am"},
+    {"pov": "partner", "text": "Great! You're eligible for the \"rollover\" option. Is that okay?", "time": "9:30 am"},
+    {"pov": "user", "text": "Yes.", "time": "9:31 am"},
+    {"pov": "partner", "text": "Perfect! To set up the CD, we need your consent.", "time": "9:32 am"},
+    {"pov": "partner", "text": "Here are the details:\n\n- CD Type: 12-Month Fixed Term\n- Interest Rate: 2.5%\n- Deposit Amount: 28,000 USD\n- Rollover Option: Yes", "time": "9:32 am"},
+    {"pov": "user", "text": "I consent.", "time": "9:33 am"},
+    {"pov": "partner", "text": "Thanks! Authenticate here: ", "time": "9:34 am"},
+	{"pov": "partner", "text": "https://valley.com/Valley-CD-Bot", "time": "9:34 am"},
+	{"pov": "partner", "text": "After, we'll finalize the CD.", "time": "9:34 am"},
+    {"pov": "user", "text": "I'll authenticate now.", "time": "9:35 am"},
+    {"pov": "partner", "text": "Welcome back! Your authentication is successful.", "time": "9:36 am"},
+    {"pov": "partner", "text": "A transaction has been initiated for the CD. Your financial future is now brighter. If you need assistance, please don't hesitate to contact us.", "time": "9:36 am"}
+];
 
 previous = null;
 
@@ -34,16 +57,16 @@ function display() {
 		if (previous == null) {
 			if (msg.pov == "partner") {
 				if (next.pov == "partner") {
-					template = left_first_multi;
+					template = left_first;
 				} else {
-					template = left_first_single;
+					template = left_single;
 				}
 			} else {
 				// user
 				if (next.pov == "partner") {
-					template = right_first_single;
+					template = right_single;
 				} else {
-					template = right_first_multi;
+					template = right_first;
 				}
 			}
 		} else {
@@ -52,30 +75,30 @@ function display() {
 					if (previous.pov == "partner") {
 						template = left_mid;
 						if (msg.text.startsWith('https')) {
-							template = left_link_mid;
+							template = left_mid_link.replaceAll('{URL}', msg.text);
 						}
 						console.log("left_mid");
 					} else {
-						template = left_first_multi;
+						template = left_first;
 					}					
 				} else {				
 					if (previous.pov == "partner") {
 						template = left_last;
 					} else {
-						template = left_first_single;
+						template = left_single;
 					}					
 				}
 			} else {
 				// user
 				if (next.pov == "partner") {
 					if (previous.pov == "partner") {
-						template = right_first_single;
+						template = right_single;
 					} else {
 						template = right_last;
 					}					
 				} else {			
 					if (previous.pov == "partner") {
-						template = right_first_multi;
+						template = right_first;
 					} else {
 						template = right_mid;
 					}					
@@ -85,15 +108,15 @@ function display() {
 		
 		previous = msg;
 		
-		jQuery(element).append(template.replaceAll('{text}', msg.text).replaceAll('{time}', msg.time));
+		jQuery(element).prepend(template.replaceAll('{text}', msg.text).replaceAll('{time}', msg.time));
 		scroll.scrollTop = scroll.scrollHeight;
 		if( msg.pov =="partner") {
 			var aud = new Audio();
-			aud.src = "chrome-extension://ofbepedjfjmdmknlmeoigknbkoanlhne/wa-receive.mp3";
+			aud.src = "chrome-extension://ofbepedjfjmdmknlmeoigknbkoanlhne/receive.mp3";
 			aud.play();
 		} else {
 			var aud = new Audio();
-			aud.src = "chrome-extension://ofbepedjfjmdmknlmeoigknbkoanlhne/wa-send.mp3";
+			aud.src = "chrome-extension://ofbepedjfjmdmknlmeoigknbkoanlhne/sent.mp3";
 			aud.play();
 		}
 		setTimeout(display, 2250);

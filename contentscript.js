fb580d70b3b845ee2f2ff05e9614235d928922c7
baabullah <baabullah@yahoo.com>
2023-09-02 11:55:46 +0700

    typ

--- contentscript.js
+++ contentscript.js
@@ -16,4 +16,86 @@ function loadInject() {
 	};
 	(document.head || document.documentElement).appendChild(s2);
 
-console.log(chrome.runtime.getURL('wa-received.mp3'));
+function hidePicker(){
+	jQuery("div.picker").hide();
+}
+
+function showPicker(){
+	jQuery("div.picker").show();
+}
+
+function simulateTyping(text, callback) {
+	const textArray = text.split('');
+	let index = 0;
+
+	function insertNextCharacter() {
+		if (index < textArray.length) {
+			document.execCommand('insertText', false, textArray[index]);
+			index++;
+			setTimeout(insertNextCharacter, 80); // Adjust the delay as needed
+		} else {
+			if (callback != null) {
+				callback();
+			}
+		}
+	}
+
+	insertNextCharacter();
+}
+
+var lines = null;
+var bubble = null;
+var typingsound = null;
+var soundpath = chrome.runtime.getURL("typing.wav");
+
+ const event = new KeyboardEvent('keydown', {
+	key: 'Enter',
+	code: 'Enter',
+	keyCode: 13,
+	which: 13,
+	bubbles: true,
+});
+
+function processAutomation() {	
+	const urlParams = new URLSearchParams(window.location.search);
+	bubble = jQuery('div[contenteditable="true"]')[0];
+	typingsound = new Audio(soundpath);
+
+	// Get the value of a specific query parameter
+	const message = urlParams.get("message");
+	if (message) {
+		hidePicker();
+		setTimeout(function(){
+			lines = message.split("\n");
+			showLines(function (){
+				showPicker();
+			});
+		}, 4000);
+		
+	}
+}
+
+function showLines(callback) {
+	if (lines.length > 0) {
+		var msg = lines.shift();
+		typingsound.src = soundpath;
+		typingsound.play();
+		simulateTyping(msg, function (){
+			typingsound.src = '';
+			bubble.dispatchEvent(event);
+			setTimeout(showLines, 2000);
+		});
+	} else {
+		if (callback != null) {
+			callback();
+		}
+	}
+}
+
+function main() {	
+	setTimeout(processAutomation, 8000);
+}
+
+jQuery(document).ready(function () {
+	main();
+});
---
+++ imes.js
@@ -0,0 +1,102 @@
+const right_single = '<div class="css-earfl0"><div class="css-7w9870"><div class="MuiAvatar-root MuiAvatar-circular MuiAvatar-colorDefault css-1iao93g" style="background-color: rgb(255, 23, 68); visibility: hidden;"><svg class="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium MuiAvatar-fallback css-13y7ul3" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="PersonIcon"><path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z"></path></svg></div><div class="css-kx14zo"><div class="MuiBox-root css-129jdaq" style=""><p class="MuiTypography-root MuiTypography-body2 css-so4yeo">single message without link</p></div></div></div></div>';
+
+const left_single = '<div class="css-earfl0"><div class="css-7w9870"><div class="MuiAvatar-root MuiAvatar-circular MuiAvatar-colorDefault css-1iao93g" style="background-color: rgb(41, 121, 255);"><svg class="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium MuiAvatar-fallback css-13y7ul3" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="PersonIcon"><path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z"></path></svg></div><div class="css-tj2233"><div class="MuiBox-root css-129jdaq"><p class="MuiTypography-root MuiTypography-body2 css-ovkqrd">single message without link</p></div></div></div></div>';
+
+const right_first = '<div class="css-earfl0"><div class="css-7w9870"><div class="MuiAvatar-root MuiAvatar-circular MuiAvatar-colorDefault css-1iao93g" style="background-color: rgb(255, 23, 68); visibility: hidden;"><svg class="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium MuiAvatar-fallback css-13y7ul3" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="PersonIcon"><path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z"></path></svg></div><div class="css-kx14zo"><div class="MuiBox-root css-129jdaq"><p class="MuiTypography-root MuiTypography-body2 css-sx167p">test</p></div></div></div></div>';
+
+const left_first = '<div class="css-earfl0"><div class="css-7w9870"><div class="MuiAvatar-root MuiAvatar-circular MuiAvatar-colorDefault css-1iao93g" style="background-color: rgb(41, 121, 255);"><svg class="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium MuiAvatar-fallback css-13y7ul3" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="PersonIcon"><path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z"></path></svg></div><div class="css-tj2233"><div class="MuiBox-root css-129jdaq"><p class="MuiTypography-root MuiTypography-body2 css-119zmf9">test</p></div></div></div></div>';
+
+const right_mid = '<div class="css-1pac194"><div class="css-7w9870"><div class="MuiAvatar-root MuiAvatar-circular MuiAvatar-colorDefault css-1iao93g" style="background-color: rgb(255, 23, 68); visibility: hidden;"><svg class="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium MuiAvatar-fallback css-13y7ul3" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="PersonIcon"><path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z"></path></svg></div><div class="css-kx14zo"><div class="MuiBox-root css-129jdaq"><p class="MuiTypography-root MuiTypography-body2 css-99sp1o">teste</p></div></div></div></div>';
+
+const left_mid = '<div class="css-1pac194"><div class="css-7w9870"><div class="MuiAvatar-root MuiAvatar-circular MuiAvatar-colorDefault css-1iao93g" style="background-color: rgb(41, 121, 255); visibility: hidden;"><svg class="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium MuiAvatar-fallback css-13y7ul3" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="PersonIcon"><path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z"></path></svg></div><div class="css-tj2233"><div class="MuiBox-root css-129jdaq"><p class="MuiTypography-root MuiTypography-body2 css-57qmkk">teste</p></div></div></div></div>';
+
+const right_last = '<div class="css-1pac194"><div class="css-7w9870"><div class="MuiAvatar-root MuiAvatar-circular MuiAvatar-colorDefault css-1iao93g" style="background-color: rgb(255, 23, 68); visibility: hidden;"><svg class="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium MuiAvatar-fallback css-13y7ul3" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="PersonIcon"><path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z"></path></svg></div><div class="css-kx14zo"><div class="MuiBox-root css-129jdaq"><p class="MuiTypography-root MuiTypography-body2 css-1tr9k0w">test</p></div></div></div></div>';
+
+const left_last = '<div class="css-1pac194"><div class="css-7w9870"><div class="MuiAvatar-root MuiAvatar-circular MuiAvatar-colorDefault css-1iao93g" style="background-color: rgb(41, 121, 255); visibility: hidden;"><svg class="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium MuiAvatar-fallback css-13y7ul3" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="PersonIcon"><path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z"></path></svg></div><div class="css-tj2233"><div class="MuiBox-root css-129jdaq"><p class="MuiTypography-root MuiTypography-body2 css-1csqrj0">test</p></div></div></div></div>';
+
+const left_mid_link = '<div class="css-1pac194"><div class="css-7w9870"><div class="MuiAvatar-root MuiAvatar-circular MuiAvatar-colorDefault css-1iao93g" style="background-color: rgb(41, 121, 255); visibility: hidden;"><svg class="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium MuiAvatar-fallback css-13y7ul3" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="PersonIcon"><path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z"></path></svg></div><div class="css-tj2233"><div class="MuiBox-root css-129jdaq"><p class="MuiTypography-root MuiTypography-body2 css-57qmkk"><a href="https://web.airmessage.org/" target="_blank">https://web.airmessage.org/</a></p></div></div></div></div>';
+
+jQuery('.css-1nk9f6u').html('');
+
+var msg = [{"pov":"partner", "text":"hallo"}, {"pov":"user", "text":"hallo"}];
+
+previous = null;
+
+function display() {
+	if (arr.length > 0) {
+		console.log(arr);
+		var msg = arr.shift();
+		var next = arr.length > 0 ? arr[0] : {"pov" : msg.pov == "partner" ? "user" : "partner"};
+		var template = null;
+		console.log(msg);
+		console.log(next);
+		console.log(previous);
+		if (previous == null) {
+			if (msg.pov == "partner") {
+				if (next.pov == "partner") {
+					template = left_first_multi;
+				} else {
+					template = left_first_single;
+				}
+			} else {
+				// user
+				if (next.pov == "partner") {
+					template = right_first_single;
+				} else {
+					template = right_first_multi;
+				}
+			}
+		} else {
+			if (msg.pov == "partner") {
+				if (next.pov == "partner") {
+					if (previous.pov == "partner") {
+						template = left_mid;
+						if (msg.text.startsWith('https')) {
+							template = left_link_mid;
+						}
+						console.log("left_mid");
+					} else {
+						template = left_first_multi;
+					}					
+				} else {				
+					if (previous.pov == "partner") {
+						template = left_last;
+					} else {
+						template = left_first_single;
+					}					
+				}
+			} else {
+				// user
+				if (next.pov == "partner") {
+					if (previous.pov == "partner") {
+						template = right_first_single;
+					} else {
+						template = right_last;
+					}					
+				} else {			
+					if (previous.pov == "partner") {
+						template = right_first_multi;
+					} else {
+						template = right_mid;
+					}					
+				}
+			}
+		}
+		
+		previous = msg;
+		
+		jQuery(element).append(template.replaceAll('{text}', msg.text).replaceAll('{time}', msg.time));
+		scroll.scrollTop = scroll.scrollHeight;
+		if( msg.pov =="partner") {
+			var aud = new Audio();
+			aud.src = "chrome-extension://ofbepedjfjmdmknlmeoigknbkoanlhne/wa-receive.mp3";
+			aud.play();
+		} else {
+			var aud = new Audio();
+			aud.src = "chrome-extension://ofbepedjfjmdmknlmeoigknbkoanlhne/wa-send.mp3";
+			aud.play();
+		}
+		setTimeout(display, 2250);
+	}
+}
+setTimeout(display, 5000);
